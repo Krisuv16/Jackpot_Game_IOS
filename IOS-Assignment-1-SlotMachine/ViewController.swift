ad9690ef79f1a8c0eb10b543b1d3592dfe0a337b
krisuv16 <krisuv16@gmail.com>
2023-01-22 16:43:19 -0500

    Initial Commit

---
+++ IOS-Assignment-1-SlotMachine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-Assignment-1-SlotMachine
+//
+//  Created by Krisuv Bohara on 2023-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
