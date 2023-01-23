5abb33679effafe05c9d975ec90dd9afc18540af
krisuv16 <krisuv16@gmail.com>
2023-01-22 19:27:18 -0500

    UI DONE

--- IOS-Assignment-1-SlotMachine/ViewController.swift
+++ IOS-Assignment-1-SlotMachine/ViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resetBt: UIButton!
+    @IBOutlet weak var exitBtn: UIButton!
+    @IBOutlet weak var spinBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resetBt.layer.cornerRadius = resetBt.bounds.width / 2
+        resetBt.clipsToBounds = true
+        exitBtn.clipsToBounds = true
+        exitBtn.layer.cornerRadius = exitBtn.bounds.width / 2
+        spinBtn.clipsToBounds = true
+        spinBtn.layer.cornerRadius = spinBtn.bounds.width / 2
         // Do any additional setup after loading the view.
     }
 
